e1bb811dd211421ded6f751f9f1f3934eb0636f8
RyotaShikano <s.r120609tennis@gmail.com>
2022-05-13 16:51:15 +0900

    [Finish]JavaScriptコードの作成、名無しを表示

--- omikuji.js
+++ omikuji.js
@@ -1,8 +1,11 @@
 let username;
 let userresult;
 
-username = prompt("お名前を教えて下さい。");
-document.getElementById("name").innerHTML = username;
+username =prompt("お名前を教えてください。");
+if(username==""){
+    username="名無し";
+}
+document.getElementById("name").innerHTML=username;
 
 let rand=Math.floor(Math.random()*5);
 if(rand==0){
